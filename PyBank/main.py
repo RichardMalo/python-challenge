1073f6367bd64acf49ce5e1a73dd5b6867d9803b
Richard Malolepszy <86073298+Rolosaregood@users.noreply.github.com>
2023-01-11 14:37:29 -0500

    Average Change added to Terminal printout

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,7 @@ monthcount = []
 nettotal = []
 Total = 0
 SumMonths = 0
+AvgChange = []
 
 with open(csvpath, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -21,8 +22,19 @@ with open(csvpath, encoding='utf') as csvfile:
         
         monthcount.append(row[0])
         nettotal.append(row[1])
-nettotal=[int(i) for i in nettotal]
-Total=sum(nettotal)
-SumMonths = len(monthcount)
-print(f"Total Months: {SumMonths}")
-print(f"Total: {Total}")
+#change netotal to an integer from ' ' to int
+nettotal = [int(x) for x in nettotal]
+#Sum up all the numbers in library nettotal
+Total = (sum(nettotal))
+#Sum up all the months by the length of the library monthcount
+SumMonths = (len(monthcount))
+#Calculating an average change of nettotal library
+AvgChange = [nettotal[x+1]-nettotal[x] for x in (range(len(nettotal)-1))]
+#Average change 
+avgchangetotal = (sum(AvgChange)/len(AvgChange))
+#Print out text to Terminal
+print("\033[1mFinancial Analysis\033[0m")
+print("------------------")
+print(f"\033[1mTotal Months: \033[0m{SumMonths}")
+print(f"\033[1mTotal: $\033[0m{Total}")
+print(f"\033[1mAverage Change: $\033[91m{round(avgchangetotal,2)}\033[0m")
